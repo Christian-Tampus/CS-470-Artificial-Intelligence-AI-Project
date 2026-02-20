20a1450f12826f0bfe4a523707f21fe8ec43b459
Christian Tampus <builderdevelite@gmail.com>
2026-02-19 22:14:06 -0600

    Updated train.py & test.py

--- test.py
+++ test.py
@@ -8,4 +8,36 @@
 
 print("[SYSTEM MESSAGE] Program Start!")
 
+#Import Dependencies
+import os
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from pathlib import Path
+
+#Declare Variables
+imageSize = 150
+currentDirectory = Path(__file__).resolve().parent
+testingSetDirectory = currentDirectory / "DataSets" / "TestingSet"
+trainingCNNModelDirectory = currentDirectory / "TrainingModels" / "CNN_Model_1.h5"
+
+#Load CNN Model
+trainingCNNModel = tf.keras.models.load_model(trainingCNNModelDirectory)
+
+#Predict Image Function
+def predictImage(directory):
+    image = Image.open(directory).resize((imageSize, imageSize))
+    imageArray = np.array(image) / 255.0
+    imageArray = np.expand_dims(imageArray, axis = 0)
+    prediction = trainingCNNModel.predict(imageArray, verbose = 0)[0][0]
+    if prediction > 0.5:
+        return "Dog"
+    return "Cat"
+
+#Test CNN Model
+for file in os.listdir(testingSetDirectory):
+    imagePath = testingSetDirectory / file
+    result = predictImage(imagePath)
+    print(f"[SYSTEM MESSAGE] Prediction For File: {file} Result: {result}")
+
 print("[SYSTEM MESSAGE] Program Terminated...")
